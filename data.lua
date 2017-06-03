c3ba21f89787ee106c68b2512389a9226675e779
Manio <atehxx@gmail.com>
2017-06-03 23:42:09 +0200

    Allow productivity modules in those recipes
    Allow productivity modules in those recipes

--- data.lua
+++ data.lua
@@ -6,3 +6,15 @@ require("prototypes.item")
 require("prototypes.recipe")
 require("prototypes.technology")
 data.raw["assembling-machine"]["oil-refinery"].fast_replaceable_group = "assembling-machine"
+
+-- Allow productivity modules in those recipes
+for i, module in pairs(data.raw.module) do
+  if module.effect.productivity then
+		table.insert(module.limitation, "specialized-refining-petroleum");
+		table.insert(module.limitation, "specialized-refining-light-oil");
+		table.insert(module.limitation, "specialized-refining-heavy-oil");
+		if not module.limitation_message_key then
+    	module.limitation_message_key = "production-module-usable-only-on-intermediates"
+    end
+  end
+end
